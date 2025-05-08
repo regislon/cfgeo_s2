0c24d6868dbe9ec5461f32dcbea65dfc34f62334
regislon <regis@lonco.ch>
2025-05-08 12:07:57 +0200

    fix: update BALISE value and enhance image handling in markdown processing

--- marp/markdown_marp_to_github.py
+++ marp/markdown_marp_to_github.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import re
 
-BALISE = "</style>"
+BALISE = "</style2>"
+IMAGE_FOLDER_NAME = "images_documentation"
 
 
 
@@ -17,6 +19,12 @@ def find_markdown_files_to_convert():
 
 def remove_text_from_markdown_files(markdown_files):
     print("Removing text from markdown files...")
+
+    list_images = []
+
+
+
+
     for file_path in markdown_files:
         print(f"Processing file: {file_path}")
         trigger = False
@@ -29,12 +37,30 @@ def remove_text_from_markdown_files(markdown_files):
                     continue
                 if trigger:
                     if line.strip() != "---":
+                        match = re.search(r'!\[.*?\]\((.*?)\)', line)
+                        if match:
+                            line = line.replace("](", f"]({IMAGE_FOLDER_NAME}/")
+
+                            list_images.append(match.group(1))
                         list_of_line.append(line)
    
         with open(file_path, 'w', encoding='utf-8') as file:
             for line in list_of_line:
                 file.write(line + '\n')
+
+        parent_folder = file_path.parent
+        for image in list_images:
+            image_path = parent_folder / image
+            if image_path.exists():
+                new_image_path = parent_folder / IMAGE_FOLDER_NAME / image
+                new_image_path.parent.mkdir(parents=True, exist_ok=True)
+                image_path.rename(new_image_path)
+                print(f"Moved {image} to {new_image_path}")
+            else:
+                print(f"Image {image} not found in {parent_folder}")
+
+        
             
 
-
+    print(list_images)
 remove_text_from_markdown_files(find_markdown_files_to_convert())
